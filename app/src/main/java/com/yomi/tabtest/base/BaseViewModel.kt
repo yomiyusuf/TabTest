6ec489cdc61a0473089659b7983807a7d76eb264
yjoseph <yomi.joseph@target.com.au>
2019-09-19 22:49:02 +1000

    Implement BaseViewModel

--- app/src/main/java/com/yomi/tabtest/base/BaseViewModel.kt
+++ app/src/main/java/com/yomi/tabtest/base/BaseViewModel.kt
@@ -1,7 +1,24 @@
 package com.yomi.tabtest.base
 
 import androidx.lifecycle.ViewModel
+import com.yomi.tabtest.injection.component.DaggerViewModelInjector
+import com.yomi.tabtest.injection.component.ViewModelInjector
+import com.yomi.tabtest.injection.module.NetworkModule
+import com.yomi.tabtest.ui.list.ListViewModel
 
 abstract class BaseViewModel: ViewModel(){
+    private val injector: ViewModelInjector = DaggerViewModelInjector
+        .builder()
+        .networkModule(NetworkModule)
+        .build()
 
+    init {
+        inject()
+    }
+
+    private fun inject(){
+        when (this) {
+            is ListViewModel -> injector.inject(this)
+        }
+    }
 }
